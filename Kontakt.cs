bf8841315f138945db33bc7807b571411aec41f7
Marko <Marko@DESKTOP-K8USG5I>
2023-05-05 12:01:50 +0200

    Add project files.

---
+++ Kontakt.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Kontakt
+    {
+        private string ime;
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+
+        private string prezime;
+
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+
+        private string glavnibroj;
+
+        public string GlavniBroj
+        {
+            get { return glavnibroj; }
+            set { glavnibroj = value; }
+        }
+
+        private List<string> ostalibrojevi = new List<string>();
+
+        public List<string> OstaliBrojevi
+        {
+            get { return ostalibrojevi; }
+            set { ostalibrojevi = value; }
+        }
+
+        public Kontakt(string ime, string prezime, string glavnibroj)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.glavnibroj= glavnibroj;
+        }
+
+        public bool DodajBroj(string dodatniBroj)
+        {
+            if (dodatniBroj == glavnibroj)
+            {
+                return false;
+            }
+
+            foreach (var item in ostalibrojevi)
+            {
+                if (dodatniBroj == item)
+                {
+                    return false;
+                }
+            }
+            ostalibrojevi.Add(dodatniBroj);
+            return true;
+            
+        }
+
+        public bool IzbrisiBroj(string dodatniBroj)
+        {
+            if (ostalibrojevi.Contains(dodatniBroj))
+            {
+                ostalibrojevi.Remove(dodatniBroj);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            str += ime;
+            str += " ";
+            str +=  prezime;
+            str += " ";
+            str += glavnibroj;
+            str += " ";
+            if (ostalibrojevi == null)
+            {
+                return str;
+            }
+            else
+            {
+                str += "[";
+                for (int i = 0; i < ostalibrojevi.Count - 1; i++)
+                {
+                    str += ostalibrojevi[i];
+                    str += ",";
+                }
+                str += ostalibrojevi[ostalibrojevi.Count - 1];
+                str += "]";
+                return str;
+            }
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kontakt marko = new Kontakt("marko","fratucan","3456134");
+            marko.DodajBroj("142442");
+            Console.WriteLine(marko);
+            marko.DodajBroj("142442");
+            Console.WriteLine(marko);
+            marko.DodajBroj("345376");
+            marko.DodajBroj("978645");
+            Console.WriteLine(marko);
+
+            Kontakt pera = new Kontakt("petar", "padejski", "6956721");
+            pera.DodajBroj("9726562");
+            Console.WriteLine(pera);
+
+            Adresar a = new Adresar();
+            a.Dodaj(marko);
+            a.Dodaj(pera);
+            string shareFajl = @"D:\DOCUNENTS\0\Documents\Visual Studio 2022\Projekti\ConsoleApp12\bin\Debug\net6.0\fajl1.txt";
+            a.Share(shareFajl, "3456134");
+
+            string shareFajl1 = @"D:\DOCUNENTS\0\Documents\Visual Studio 2022\Projekti\ConsoleApp12\bin\Debug\net6.0\fajl1.txt";
+            a.Share(shareFajl1, "00000");
+
+            string backupFajl = @"D:\DOCUNENTS\0\Documents\Visual Studio 2022\Projekti\ConsoleApp12\bin\Debug\net6.0\fajl2.txt";
+            a.Backup(backupFajl);
+
+            string receiveFajl = @"D:\DOCUNENTS\0\Documents\Visual Studio 2022\Projekti\ConsoleApp12\bin\Debug\net6.0\fajl3.txt";
+            a.Receive(receiveFajl);
+
+        }
+    }
+}
